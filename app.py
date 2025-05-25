ae91acfc22fde97f1e04d9e14af5ce58e28a444f
mhassanobaid <hassanobaidkhan999160@gmail.com>
2025-05-25 18:54:36 +0500

    hello2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello1 from Flask deployed by Jenkins!"
+    return "Hello2 from Flask deployed by Jenkins!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
